633d8da393002dfc99c4394c487909cda0aee3f8
TomVito <t.vitonis@gmail.com>
2021-11-23 20:31:15 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { ProgressbarModule } from 'ngx-bootstrap/progressbar';
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { provideDatabase,getDatabase } from '@angular/fire/database';
 import { AngularFireModule } from '@angular/fire/compat';
-import { AngularFireDatabase } from '@angular/fire/compat/database';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 import { environment } from '../environments/environment';
 
 
@@ -24,7 +24,7 @@ import { environment } from '../environments/environment';
     BrowserAnimationsModule,
     ProgressbarModule.forRoot(),
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFireDatabase
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
